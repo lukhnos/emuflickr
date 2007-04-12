7bf94880e15d020394b13af7f3f9cf5f7b6e9b38
godfat <godfat@dc71d87c-e62d-0410-9733-b73a7b6785d7>
2007-04-12 20:30:30 +0000

    it's a little to complex, reduce the complexity later. and improve to_xml to come with Flickr flavor. e.g. delete <_content>whatever</content>, attri flavor=> <something weight='100' />

--- trunk/app/helpers/EmuFlickrResponse.rb
+++ trunk/app/helpers/EmuFlickrResponse.rb
@@ -1,55 +1,41 @@
 
+require 'rubygems'
+require 'json'
+require 'active_support'
+
 class EmuFlickrResponse
   def initialize
     @data = {}
     @key = ''
   end
-  def method_missing msg, *arg, &block
+  def method_missing msg, *arg
     item = msg.to_s
     if item =~ /=$/
       @key += item.chop!
-      # array = @key.split(/\./)
-      # array[0...array.size-1].each{ |i|
-        
-        # if @data[i] == nil
-          # @data[i] = {}
-        # end
-      # }
-      @data[item] = arg
+      keys = @key.split(/\./)
+      case keys.size
+        when 1: add(lambda{|to_add| @data[keys[0]] = to_add}, @data[keys[0]], arg[0])
+        when 2: add(lambda{|to_add| (@data[keys[0]] ||= {})[keys[1]] = to_add}, (@data[keys[0]] ||= {})[keys[1]], arg[0])
+        when 3: hash_step = @data[keys[0]] ||= {}
+                keys[1...keys.size-1].each{ |i|
+                  hash_step[i] ||= {}
+                  hash_step = hash_step[i]
+                }
+                add(lambda{|to_add| hash_step[keys[-1]] = to_add}, hash_step[keys[-1]], arg[0])
+      end
       refresh
     else
       @key += item + '.'
     end
     self
   end
-  def get
-    result = @data[@key.chop]
-    refresh
-    result
-  end
-
-# <foo bar="baz" />
-
-# {
-	# "foo": {
-		# "bar": "baz"
-	# }
-# }
 
   def to_json
-    # result = '{'
-    # array = @data.to_a.sort
-    # now = ''
-    # array.each{ |i|
-      # if now == ''
-        # i.split(/./).each{ |ii|
-          # if now != ii
-            # now += ii
-            # result +=
-          # end
-        # }
-      # end
-    # }
+    @data.to_json
+  end
+  def to_xml
+    @data.to_xml
+    # modify here
   end
 
   # for debug
@@ -59,32 +45,87 @@ class EmuFlickrResponse
   def debug_data
     @data
   end
+  def debug_get
+    result = @data[@key.chop]
+    refresh
+    result
+  end
 
   private
   def refresh
     @key = ''
+    self
+  end
+  def add func, item, to_add
+    if item == nil
+      func.call to_add
+    else
+      if item.kind_of? Array
+        item << to_add
+        func.call item
+      else
+        item = [] << item << to_add
+        func.call item
+      end
+    end
   end
 end
 
+require 'test/unit'
+
+class EFRTest < Test::Unit::TestCase
 # '<auth>
 	# <token>976598454353455</token>
 	# <perms>read</perms>
 	# <user nsid="12037949754@N01" username="Bees" fullname="Cal H" />
 # </auth>'
-
-# Efr = EmuFlickrResponse.new
-# Efr.auth.token = {:_content=>'976598454353455'}
-# Efr.auth.perms = {:_content=>'read'}
-# Efr.auth.user = {:nsid=>'12037949754@N01', :username=>'Bees', :fullname=>'Cal H'}
-
-
-
+  def test_01
+    efr = EmuFlickrResponse.new
+    efr.auth.token = {:_content=>'976598454353455'}
+    efr.auth.perms = {:_content=>'read'}
+    efr.auth.user = {:nsid=>'12037949754@N01', :username=>'Bees', :fullname=>'Cal H'}
+    # puts efr.to_json
+    # puts efr.to_xml
+  end
+# <frob>746563215463214621</frob>
+  def test_02
+    efr = EmuFlickrResponse.new
+    efr.frob = {:_content=>'746563215463214621'}
+    # puts efr.to_json
+    # puts efr.to_xml
+  end
+# <photoid secret="abcdef" originalsecret="abcdef">1234</photoid>
+  def test_03
+    efr = EmuFlickrResponse.new
+    efr.photoid = {:_content=>'1234', :secret=>'abcdef', :originalsecret=>"abcdef"}
+    # puts efr.to_json
+    # puts efr.to_xml
+  end
+# <foo bar="baz">
+	# <woo yay="hoopla" />
+# </foo>
+  def test_04
+    efr = EmuFlickrResponse.new
+    efr.foo = {:bar=>'baz'}
+    efr.foo.woo = {:yay=>'hoopla'}
+    # puts efr.to_json
+    # puts efr.to_xml
+  end
+# <outer>
+	# <photo id="1" />
+	# <photo id="2" />
+# </outer>
+  def test_05
+    efr = EmuFlickrResponse.new
+    efr.outer.photo = {:id=>'1'}
+    efr.outer.photo = {:id=>'2'}
+    # puts efr.to_json
+    # puts efr.to_xml
+  end
+end
 
 # efr.data => {'auth.token'=>{:_content=>'976598454353455'}, 'auth.perm'=>{:_content=>'read'},
-#             'auth.user'=>{:nsid=>'12037949754@N01', :username='Bees', :fullname=>'Cal H'}
-
-
-# <frob>746563215463214621</frob>
+#             'auth.user'=>{:nsid=>'12037949754@N01', :username='Bees', :fullname=>'Cal H'}}
 
 # <auth>
 	# <token>976598454353455</token>
@@ -93,14 +134,10 @@ end
 # </auth>
 
 # <auth>
-	# <token>976598454353455</token>
+	# <token>9765984553455</token>
 	# <perms>write</perms>
 	# <user nsid="12037949754@N01" username="Bees" fullname="Cal H" />
 # </auth>
 
 # <photoid>1234</photoid>
-
-# <photoid secret="abcdef" originalsecret="abcdef">1234</photoid>
-
-# <ticketid>1234</ticketid>
 
